853aeb46a7cac4c53f3bbf4bb1d94f9d9352f4ac
AndressaMiyaguti <83734814+AndressaMiyaguti@users.noreply.github.com>
2022-06-06 23:57:44 -0300

    Adicionar arquivos de projeto.

--- aula01/Program.cs
+++ aula01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aula01
 {
@@ -10,6 +6,51 @@ namespace aula01
     {
         static void Main(string[] args)
         {
+            //Elabore um programa que escreve seu nome completo, seu endereço, o CEP e telefone em linhas separadas.
+/*
+            Console.WriteLine("--------------------------------- Formulário Simples" +
+                " ---------------------------------");
+
+            Console.WriteLine("Nome completo:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Meu endereço:");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Meu CEP:");
+            string cep = Console.ReadLine(); //Optei por não usar o intParse, devido ao dígito
+
+            Console.WriteLine("Meu telefone:");
+            string phone = Console.ReadLine(); //Optei por não usar o intParse, devido ao dígito
+
+            Console.WriteLine("---------------------------------Meu cadastro ---------------------------------");
+            Console.WriteLine($" Nome:{name}\n Endereço:{ address }\n CEP: { cep}\n Telefone: {phone}");
+            Console.ReadLine();
+*/
+
+
+            /*Escolha uma mulher famosa na história da tecnologia e implemente um programa que escreve seu nome, 
+             * sua formação e uma contribuição feita por ela dentro da tecnologia em linhas separadas.*/
+
+            string iconicWoman = "Grace Hopper";
+            string UniversityGraduate = "Doutorado em Matemática";
+            string contribution = "No campo da tecnologia, ela foi uma das criadoras do COBOL, uma linguagem de programação para bancos de dados comerciais. +" +
+                " Entretanto, sua história mais famosa é a que remonta à popularização do termo “bug” para indicar problemas em software. +" +
+                " Além do COBOL, Hopper também criou linguagens de programação para o UNIVAC, o primeiro computador comercial fabricado nos Estados Unidos";
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("------------------------------ Uma das mulheres que contribuíram na área de Tecnologia ------------------------------ \n\n");
+            Console.WriteLine($" Nome: {iconicWoman}\n Grau Acadêmico: {UniversityGraduate}\n" +
+                $" Contribuição tecnológica: {contribution}");
+
+            Console.ReadLine();
+
+
+
+
         }
+
     }
 }
