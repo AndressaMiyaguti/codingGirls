caece998a7efc14c648a69f65a0e73deb676a8be
AndressaMiyaguti <83734814+AndressaMiyaguti@users.noreply.github.com>
2022-06-08 14:16:44 -0300

    Organização de tarefas por funçao

--- aula02/aula02/aula02/Program.cs
+++ aula02/aula02/aula02/Program.cs
@@ -11,11 +11,22 @@ namespace aula02
     {
         static void Main(string[] args)
         {
+            Task_01();
+            Task_02();
+            Task_03();
+            Task_04();
+            Task_05();
+
+        }
+
+        static void Task_01()
+        {
             // 2.1) Faça um programa que pergunte ao usuário ano de nascimento e imprima sua idade.
+
 
             Console.WriteLine();
             Console.WriteLine("-------------------------------------- Tarefa Idade --------------------------------------\n\n");
-            Console.WriteLine("Insira o ano de seu nascimento: ");
+            Console.Write("Insira o ano de seu nascimento: ");
             Console.WriteLine();
 
 
@@ -26,25 +37,34 @@ namespace aula02
             Console.ReadLine();
 
 
+        }
+        static void Task_02()
+        {
             // 2.2) Escreva um programa que leia 10 valores inteiros e ao final exiba a soma dos números.
             //SEM TRATAMENTO DE ERRO
 
 
             Console.WriteLine();
             Console.WriteLine("-------------------------------------- Tarefa Soma --------------------------------------\n\n");
-            Console.WriteLine("Insira 10 ( dez ) em sequencia, separados por ',' números para serem somados.\nExemplo: 2,2,2,2.... ");
+            Console.Write("Insira 10 ( dez ) em sequencia, separados por ',' números para serem somados.\nExemplo: 2,2,2,2.... ");
             Console.WriteLine();
 
-        
-            string[] somar =  Console.ReadLine().Split(',');
-            int a = int.Parse(somar[0]), b= int.Parse(somar[1]), c= int.Parse(somar[2]), d= int.Parse(somar[3]),
-                e= int.Parse(somar[4]), f= int.Parse(somar[5]), g = int.Parse(somar[6]), h = int.Parse(somar[7]),
+
+            string[] somar = Console.ReadLine().Split(',');
+            int a = int.Parse(somar[0]), b = int.Parse(somar[1]), c = int.Parse(somar[2]), d = int.Parse(somar[3]),
+                e = int.Parse(somar[4]), f = int.Parse(somar[5]), g = int.Parse(somar[6]), h = int.Parse(somar[7]),
                 i = int.Parse(somar[8]), j = int.Parse(somar[9]);
 
             Console.WriteLine();
-            Console.WriteLine($"A soma dos números digitados é :  {a+b+c+d+e+f+g+h+i+j}");
+            Console.WriteLine($"A soma dos números digitados é :  {a + b + c + d + e + f + g + h + i + j}");
 
             Console.ReadLine();
+
+        }
+
+        static void Task_03()
+        {
+
 
             /* 2.3) Escreva um programa que leia o número de horas trabalhadas de um funcionário, o valor que recebe por 
              * hora e calcule o salário desse funcionário. A seguir, mostre o número de horas e o salário do funcionário, 
@@ -53,18 +73,20 @@ namespace aula02
 
             Console.WriteLine();
             Console.WriteLine("-------------------------------------- Salário --------------------------------------\n\n");
-            Console.WriteLine("\nInforme o número de horas trabalhadas ");
+            Console.Write("\nInforme o número de horas trabalhadas ");
 
             int hours = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nInforme o valor por hora trabalhada, que o usuário recebe. ");
+            Console.Write("\nInforme o valor por hora trabalhada, que o usuário recebe. ");
 
             float wage = float.Parse(Console.ReadLine());
 
             Console.WriteLine($"Quantidade de horas trabalhadas: {hours}\nSalário final recebido: R$ {(hours * wage).ToString("F2", CultureInfo.InvariantCulture) }");
             Console.ReadLine();
+        }
 
-
+        static void Task_04()
+        {
             /* 2.4) Leia um valor inteiro correspondente à idade de uma pessoa e mostre-a em anos, meses e dias.
              * Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
              * Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
@@ -74,7 +96,7 @@ namespace aula02
 
             Console.WriteLine();
             Console.WriteLine("-------------------------------------- Idade --------------------------------------\n\n");
-            Console.WriteLine("\nInforme sua idade: ");
+            Console.Write("\nInforme sua idade: ");
             int years = int.Parse(Console.ReadLine());
 
             int month = 12 * years;
@@ -83,11 +105,15 @@ namespace aula02
 
             Console.WriteLine("--------------------------------------Com variáveis--------------------------------------\n\n");
             Console.WriteLine($" Olá, sua idade é {years} anos\n Representando {month} meses ou\n {days} dias de idade! ");
-            
+
             Console.WriteLine("--------------------------------------Sem variáveis--------------------------------------\n\n");
             Console.WriteLine($" Olá, sua idade é {years} anos\n Representando {years * 12} meses ou\n {years * 365} dias de idade! ");
 
             Console.ReadLine();
+        }
+
+        static void Task_05()
+        {
 
             /* 2.5) Construa um conversor de moedas:
             * Crie um programa que solicite um valor em real ao usuário e converta esse valor, para:
@@ -100,9 +126,9 @@ namespace aula02
             * Para esse exercício você precisará realizar uma pesquisa para saber a cotação de cada moeda em real. 
             * Mostrar o resultado no formato símbolo da moeda e valor, como o exemplo R$ 10,00 para a moeda real.*/
 
-            Console.WriteLine();
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("-------------------------------------- Conversor --------------------------------------\n\n");
-            Console.WriteLine("\nInforme o valor em R$(real) para ser convertido. ");
+            Console.Write("\nInforme o valor em R$(real) para ser convertido: ");
 
             float value = float.Parse(Console.ReadLine());
 
@@ -118,28 +144,29 @@ namespace aula02
             float argentinianPeso = 0.040f;
             float chileanPeso = 0.0059f;
 
-            if(choice == 1)
+            if (choice == 1)
             {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Dolar US$ {(value / dolar).ToString("F2",CultureInfo.InvariantCulture)}.");
+                Console.WriteLine($" Opção: 1\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Dolar US$ {(value / dolar).ToString("F2", CultureInfo.InvariantCulture)}.");
             }
-            else if( choice == 2)
+            else if (choice == 2)
             {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Libras Esterlina £ {(value / libra).ToString("F2", CultureInfo.InvariantCulture)}.");
+                Console.WriteLine($" Opção: 2\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Libras Esterlina £ {(value / libra).ToString("F2", CultureInfo.InvariantCulture)}.");
             }
-            else if ( choice == 3)
+            else if (choice == 3)
             {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Dolar Canadense CAD {(value / canadianDollar).ToString("F2", CultureInfo.InvariantCulture)}.");
+                Console.WriteLine($" Opção: 3\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Dolar Canadense CAD {(value / canadianDollar).ToString("F2", CultureInfo.InvariantCulture)}.");
             }
             else if (choice == 4)
             {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Peso Argentino ARS {(value / argentinianPeso).ToString("F2", CultureInfo.InvariantCulture)}.");
+                Console.WriteLine($" Opção: 4\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Peso Argentino ARS {(value / argentinianPeso).ToString("F2", CultureInfo.InvariantCulture)}.");
             }
-            else if (choice == 5) {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Peso Chileno CLP {(value / chileanPeso).ToString("F2", CultureInfo.InvariantCulture)}.");
-            }
-            else if ( choice == 6)
+            else if (choice == 5)
             {
-                Console.WriteLine($" Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Euro € {(value / euro).ToString("F2", CultureInfo.InvariantCulture)}.");
+                Console.WriteLine($" Opção: 5\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Peso Chileno CLP {(value / chileanPeso).ToString("F2", CultureInfo.InvariantCulture)}.");
+            }
+            else if (choice == 6)
+            {
+                Console.WriteLine($" Opção: 6\n Valor em real R$ {value.ToString("F2", CultureInfo.InvariantCulture)}\n Valor convertido para Euro € {(value / euro).ToString("F2", CultureInfo.InvariantCulture)}.");
             }
             else
             {
